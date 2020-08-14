04079bb25ce8ae578e8044bbbfba1474835c29de
minguw <598251106@qq.com>
2020-08-14 23:08:08 +0800

    pass error_handling move_semantics option

--- exercises/option/option2.rs
+++ exercises/option/option2.rs
@@ -6,11 +6,17 @@
 fn main() {
     let optional_value = Some(String::from("rustlings"));
     // TODO: Make this an if let statement whose value is "Some" type
-    value = optional_value {
-        println!("the value of optional value is: {}", value);
-    } else {
+    if let value = optional_value{
+        println!("the value of optional value is: {:?}", value);
+    }else{
         println!("The optional value doesn't contain anything!");
     }
+    // value = optional_value {
+    //     println!("the value of optional value is: {}", value);
+    // } else {
+    //     println!("The optional value doesn't contain anything!");
+    // }
+
 
     let mut optional_values_vec: Vec<Option<i8>> = Vec::new();
     for x in 1..10 {
@@ -19,7 +25,8 @@ fn main() {
 
     // TODO: make this a while let statement - remember that vector.pop also adds another layer of Option<T>
     // You can stack `Option<T>`'s into while let and if let
-    value = optional_values_vec.pop() {
-        println!("current value: {}", value);
+    while let Some(value) = optional_values_vec.pop() {
+        println!("current value: {:?}", value);
+
     }
 }
